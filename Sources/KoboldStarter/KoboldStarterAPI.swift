d4f4de124c00e8783fde72686e463c213c2e90fd
Andy Stanton <andystanton@users.noreply.github.com>
2026-02-26 23:54:11 +0000

    Fix macro name

--- Sources/KoboldStarter/KoboldStarterAPI.swift
+++ Sources/KoboldStarter/KoboldStarterAPI.swift
@@ -3,4 +3,4 @@ import SwiftSyntaxMacros
 #endif
 
 @attached(peer, names: named(kobold_register_app))
-public macro KoboldMain() = #externalMacro(module: "KoboldStarterImpl", type: "KoboldStarterMacro")
+public macro KoboldStarter() = #externalMacro(module: "KoboldStarterImpl", type: "KoboldStarterMacro")
